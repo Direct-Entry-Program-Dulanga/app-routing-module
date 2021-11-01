cd5ab4903733882bfc44e58db20aab525783a786
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-11-01 23:55:54 +0530

    add subscribe and navigationEnd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'routing-demo';
+  currentRoute = 'Dashboard';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+
+      if (event instanceof NavigationEnd){
+        switch(event.urlAfterRedirects){
+          case "/dashboard":
+            this.currentRoute = 'Dashboard'
+            break;
+          case "/manage-customer":
+            this.currentRoute= 'Manage Customers';
+            break;
+          case "/manage-item":
+            this.currentRoute = 'Manage Items';
+            break;
+          case "/place-order":
+            this.currentRoute = 'Place Order';
+            break;
+          case "/search-order":
+            this.currentRoute = 'Search Orders';
+            break;
+          default:
+            this.currentRoute = "Not Found";
+            break;
+        }
+      }
+    });
+  }
 }
--- src/app/manage-customers/manage-customers.component.ts
+++ src/app/manage-customers/manage-customers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-manage-customers',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageCustomersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  navigate() {
+    // this.router.navigate(['/dashboard']);
+    this.router.navigateByUrl('/dashboard');
   }
 
 }
