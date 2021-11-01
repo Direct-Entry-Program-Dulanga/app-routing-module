e505db7cbd52a2f1b759539d9fd1a5463d8468e5
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-11-01 09:40:31 +0530

    Create Routes for all pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {ManageCustomersComponent} from "./manage-customers/manage-customers.component";
+import {ManageItemsComponent} from "./manage-items/manage-items.component";
+import {PlaceOrderComponent} from "./place-order/place-order.component";
+import {SearchOrdersComponent} from "./search-orders/search-orders.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'manage customers',
+    component: ManageCustomersComponent
+  },
+  {
+    path: 'manage items',
+    component: ManageItemsComponent
+  },
+  {
+    path: 'place order',
+    component: PlaceOrderComponent
+  },
+  {
+    path: 'search order',
+    component: SearchOrdersComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
